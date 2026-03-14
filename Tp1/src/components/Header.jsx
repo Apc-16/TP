eb92c7fbe5a3013dcf5a19ab05aa557712f6833d
Apc-16 <2295286@cegepmv.ca>
2026-03-14 17:19:58 -0400

    Update 2

--- Tp1/src/components/Header.jsx
+++ Tp1/src/components/Header.jsx
@@ -3,11 +3,13 @@ import Image from './e888cd8d2708d8f0388198fd551a1a00.jpg';
 
 function Header() {
     return (
-    <header classname="header">
-            <div classname="header-logo">
+    <header className="header">
+            <div className="header-logo">
                 <img src={Image} alt="site logo" />
             </div>
             <h1>MySocial</h1>
         </header>
     )
 }
+
+export default Header
---
+++ Tp1/src/components/ProfileSection.jsx
@@ -0,0 +1,56 @@
+import './ProfileSection.css'
+import Image from './Tp1/src/components/Capture d’écran 2026-03-14 155556.png';
+
+function ProfileSection() {
+    return (
+    <div className="profile">
+                <NameProfile />
+                <BioProfile />
+                <StatsProfile />
+            </div>
+
+    );
+}
+
+function NameProfile() {
+return (
+   <div className="profile-name-image">
+    <img src={profile.photoProfile} alt="Profile-image" />
+       <h1 className="profile-user-name">
+        {profile.prenom} {profile.nom}
+        </h1> 
+    </div> 
+);
+}
+
+function BioProfile() {
+    return <div className="profile-bio">
+        <p>{profile.bio}</p>
+    </div>
+}
+
+function StatsProfile () {
+    return (
+        <div className="profile-stats">
+            <ul>
+                <li>{profile.nbrPoste}</li>
+                <li>{profile.nbrAbonnes}</li>
+                <li>{profile.nbrAbonnement}</li>
+            </ul>
+        </div>
+
+    );
+}
+
+
+const profile = {
+    photoProfile: './Tp1/src/components/Capture d’écran 2026-03-14 155556.png',
+    nom: "Doe",
+    prenom: "Jane",
+    bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit 📷✈️🏕️", 
+    nbrPoste: 4,
+    nbrAbonnes: 188,
+    nbrAbonnement: 206,
+}
+
+
